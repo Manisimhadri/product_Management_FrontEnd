ae35bf9a2cf71d10ff1eb53e75cf08fa770f9c1f
Mani Anil Kumar Simhadri <21A91A05J0@aec.edu.in>
2024-07-10 12:21:58 +0530

    frontend

---
+++ src/component/EditProduct.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import productService from '../service/product.service';
+import { useNavigate, useParams } from 'react-router-dom';
+
+function EditProduct() {
+    const [product, setProduct] = useState({
+        id: "",
+        productName: "",
+        description: "",
+        price: "",
+        status: "",
+    });
+
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const [message, setMsg] = useState("");
+
+    useEffect(() => {
+        productService.getProductById(id).then((res) => {
+            setProduct(res.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, [id]);
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setProduct({ ...product, [e.target.name]: value });
+    };
+
+    const ProductUpdate = (e) => {
+        e.preventDefault();
+
+        productService.editProduct(product).then((res) => {
+            setMsg("Product updated successfully!");
+            navigate("/");
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
+    return (
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <div className="card">
+                        <div className="card-header fs-3 text-center">
+                            Edit Product
+                        </div>
+                        {message && 
+                            <p className="fs-4 text-center text-success">{message}</p>                            
+                        }
+                        <div className="card-body">
+                            <form onSubmit={ProductUpdate}>
+                                <div className="mb-3">
+                                    <label htmlFor="productName">Enter Product Name</label>
+                                    <input
+                                        type="text"
+                                        name="productName"
+                                        className="form-control"
+                                        onChange={handleChange}
+                                        value={product.productName}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label htmlFor="description">Enter Description</label>
+                                    <input
+                                        type="text"
+                                        name="description"
+                                        className="form-control"
+                                        onChange={handleChange}
+                                        value={product.description}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label htmlFor="price">Enter Price</label>
+                                    <input
+                                        type="text"
+                                        name="price"
+                                        className="form-control"
+                                        onChange={handleChange}
+                                        value={product.price}
+                                    />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label htmlFor="status">Enter Status</label>
+                                    <input
+                                        type="text"
+                                        name="status"
+                                        className="form-control"
+                                        onChange={handleChange}
+                                        value={product.status}
+                                    />
+                                </div>
+                                <button className="btn btn-primary col-md-12">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EditProduct;
